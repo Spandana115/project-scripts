e1605b2947aee5bf9db7709bd5e435175f3f1845
Spandana115 <baindlaspandana0115@gmail.com>
2025-01-07 16:59:56 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install python3 -y
-yum install python3-pip -y
+sudo yum install python3-pip -y
 sudo yum install git -y
-git clone https://github.com/Spandana115/car-prediction.git
-sudo mv car-prediction/ /home/ec2-user/
-cd /home/ec2-user/car-prediction
+git clone https://github.com/Spandana115/flask-library-app.git
+sudo mv flask-library-app/ /home/ec2-user/
+cd /home/ec2-user/flask-library-app
 pip3 install -r requirements.txt
-python3 ./app.py
+nohup python3 ./app.py &
