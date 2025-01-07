230801ee298e2fce93ff0f0259745926cc4c39f6
root <root@ip-172-31-37-7.ec2.internal>
2025-01-07 10:30:47 +0000

    commiting files

---
+++ data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install git -y
+git clone https://github.com/Spandana115/car-prediction.git
+sudo mv car-prediction /home/ec2-user/
+cd /home/ec2-user/car-prediction
+sudo yum install python3 -y
+yum install python3-pip -y
+pip3 install -r requirements.txt
+screen -m -d python3 ./app.py
---
+++ ec2.tf
@@ -0,0 +1,48 @@
+resource "aws_instance" "public_subnet-1" {
+ami="ami-0ca9fb66e076a6e32"
+instance_type="t2.micro"
+count=1
+key_name="spandana"
+vpc_security_group_ids=["${aws_security_group.demosg.id}"]
+subnet_id="${aws_subnet.public_subnet-1.id}"
+associate_public_ip_address=true
+user_data="${file("data.sh")}"
+tags={
+Name="My public Instance 1 "
+}
+}
+
+resource "aws_security_group" "demosg" {
+vpc_id="${aws_vpc.demovpc.id}"
+
+ingress{
+from_port=80
+to_port=80
+protocol="tcp"
+cidr_blocks=["0.0.0.0/0"]
+}
+
+ingress{
+from_port=8000
+to_port=8000
+protocol="tcp"
+cidr_blocks=["0.0.0.0/0"]
+}
+
+ingress{
+from_port=22
+to_port=22
+protocol="tcp"
+cidr_blocks=["0.0.0.0/0"]
+}
+
+egress{
+from_port=0
+to_port=0
+protocol="-1"
+cidr_blocks=["0.0.0.0/0"]
+}
+tags={
+Name="WEB SG"
+}
+}
